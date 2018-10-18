985c03e0b9a2c854763be93a0400e999b7c688e8
Thomas <bischoff1870@gmail.com>
2018-10-18 11:11:04 -0500

    Initial commit

---
+++ Unit 5 Quiz.ps1
@@ -0,0 +1,18 @@
+﻿read-host -prompt "input a number"
+if (10, -10) write-host -NoNewline
+$timevariable = get-process system
+foreach ($object in $timevariable) {
+out-printer write-output}
+
+
+
+
+#Ask a user to input a number and using if statements tell the user if it is higher or lower then a number you pick out.
+
+
+
+#Using Get-Process get only the StartTime property and store it in a variable
+
+#Using a loop print each line of the variable you stored the start times in, and have it print the start time each time the loop runs and a message telling the user what it is outputting.
+
+#Using git make a local copy of this repository https://github.com/PowerShell/PowerShell
